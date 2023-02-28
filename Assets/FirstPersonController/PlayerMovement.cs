fc00299570fb82b3d84bc76cb1e793b621a287f1
temaxuck <gorropand@gmail.com>
2023-03-01 03:12:39 +0700

    Enhanced Jump mechanics: made it more `realistic`

--- Assets/FirstPersonController/PlayerMovement.cs
+++ Assets/FirstPersonController/PlayerMovement.cs
@@ -6,17 +6,18 @@ public class PlayerMovement : MonoBehaviour
 {
     public CharacterController controller;
 
-    public float movementSpeed = 12f;
+    public float movementSpeed = 6f;
     public float gravity = -9.81f * 3f;
-    public float jumpHeight = 2f;
 
     public Transform groundCheck;
     public float groundDistance = 0.4f;
     public LayerMask groundMask;
     private bool isGrounded;
 
+    public float jumpHeight = 2f;
+    private float jumpForce;
+    private string jumpDirection;
     private Vector3 velocity;
-
 
     // Start is called before the first frame update
     void Start()
@@ -26,16 +27,31 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float InputX = Input.GetAxis("Horizontal"),
+              InputZ = Input.GetAxis("Vertical");
+        float absInputX = Mathf.Abs(InputX),
+              absInputZ = Mathf.Abs(InputZ);
+        Vector3 move;  
+
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
         if (isGrounded && velocity.y < 0) {
             velocity.y = -2f;
+            if (absInputX > absInputZ) {
+                jumpDirection = "x";
+                jumpForce = InputX;
+            } else {
+                jumpDirection = "z";
+                jumpForce = InputZ;
+            } 
+        } 
+
+        if (!isGrounded) {
+            // Player is floating
+            move = jumpDirection == "x" ? (transform.right * jumpForce) : (transform.forward * jumpForce);
+        } else {
+            move = transform.right * InputX + transform.forward * InputZ;
         }
-
-        float x = Input.GetAxis("Horizontal"),
-              z = Input.GetAxis("Vertical");
-
-        Vector3 move = transform.right * x + transform.forward * z;  
 
         controller.Move(move * movementSpeed * Time.deltaTime);
 
