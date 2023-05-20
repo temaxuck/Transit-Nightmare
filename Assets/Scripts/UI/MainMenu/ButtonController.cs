6bc16f702ae9546ac3d4f4b3e3fcdf6af89bc86a
temaxuck <gorropand@gmail.com>
2023-05-20 15:50:23 +0700

    Added exit to main menu on press escape key feature

--- Assets/Scripts/UI/MainMenu/ButtonController.cs
+++ Assets/Scripts/UI/MainMenu/ButtonController.cs
@@ -1,14 +1,18 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ButtonController : MonoBehaviour {
+public class ButtonController : MonoBehaviour 
+{
     [SerializeField] private Button[] buttons;
     public int focusedButtonIndex = 0;
 
-    void Update() {
-        if (Input.GetKeyDown(KeyCode.UpArrow)) {
+    void Update() 
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
             ChangeFocus(-1);
-        } else if (Input.GetKeyDown(KeyCode.DownArrow)) {
+        } else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
             ChangeFocus(1);
         } else if (Input.GetKeyDown(KeyCode.Return))
         {
@@ -16,13 +20,15 @@ public class ButtonController : MonoBehaviour {
         }
     }
 
-    private void ChangeFocus(int direction) {
+    private void ChangeFocus(int direction) 
+    {
         buttons[focusedButtonIndex].OnDeselect(null);
         focusedButtonIndex = (focusedButtonIndex + direction + buttons.Length) % buttons.Length;
         buttons[focusedButtonIndex].Select();
     }
 
-    private void ActivateButton() {
+    private void ActivateButton() 
+    {
         buttons[focusedButtonIndex].onClick.Invoke();
     }
 }
